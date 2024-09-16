10308e6e516e85d07dc11545ced36f6598fedbb1
Marelin0222 <marelynuluan@gmail.com>
2024-09-16 10:10:50 -0600

    Add files via upload

---
+++ IPC1_PRACTICA1_202202069.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ipc_practica1_202202069;
+
+/**
+ *
+ * @author HP
+ */
+public class IPC1_PRACTICA1_202202069 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
